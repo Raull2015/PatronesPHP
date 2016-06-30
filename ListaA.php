51ed3cd8b60751007f0929c827b68d4f9b610763
Edwin <ed6104596@gmail.com>
2016-06-29 20:46:11 -0600

    implementando interfaz

--- ListaA.php
+++ ListaA.php
@@ -1,7 +1,9 @@
 <?php
 
-	class ListaA {
+	class ListaA implements Observado{
 		$lista = array();
+		$observadores = array();
+
 		$op;
 
 		public function agregarArray($lista1){
@@ -15,5 +17,18 @@
 		public function sepOp($op2){
 			$op  = $op2;
 		}
+
+		public function agregarObservador($b){
+			array_push($observadores, $b)
+		};
+  		public function quitarObservador($b1){
+
+  		};
+
+  		public function notificar(){
+  			foreach ($observadores as $interador => $pos) {
+  				$pos -> 
+  			}
+  		};
 	}
 ?>
